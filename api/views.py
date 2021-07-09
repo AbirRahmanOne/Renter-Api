5a5e461bacaab25d180cd98e2fda83f697671f1e
AbirRahmanOne <abir.talks@gmail.com>
2021-07-09 22:45:08 +0600

    flat api update with default errors

--- api/views.py
+++ api/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
-from django.http import HttpResponse , JsonResponse
+from django.http import HttpResponse, JsonResponse
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 from .serializers import *
 from .models import *
+
+import logging
+from rest_framework.views import exception_handler
+from django.http import JsonResponse
+from requests import ConnectionError
+
+
 # Create your views here.
 
 
@@ -13,36 +20,77 @@ def index(request):
     return HttpResponse('Hello, we are in RMS server!')
 
 
+def custom_404(request, exception=None):
+    return JsonResponse({
+        'status_code': 404,
+        'error': 'The resource was not found'
+    },
+        status=status.HTTP_404_NOT_FOUND
+
+    )
+
+
 @api_view(['GET'])
 def flat_list(request):
     try:
-        flats = Flat.objects.all()
-        data = FlatSerializer(flats, many=True).data
-        return Response(data, status=status.HTTP_200_OK)
+        flats = Flat.objects.all().order_by('name')
+        serializer = FlatSerializer(flats, many=True).data
+        return Response(serializer, status=status.HTTP_200_OK)
     except:
         return Response(status=status.HTTP_400_BAD_REQUEST)
+
 
 @api_view(['GET'])
 def flat_info(request, pk):
     try:
         flat = Flat.objects.get(id=pk)
-        data = FlatSerializer(flat, many=False).data
-        return Response(data, status=status.HTTP_200_OK)
+        serializer = FlatSerializer(flat, many=False).data
+        return Response(serializer, status=status.HTTP_200_OK)
     except:
-        data = { "Message": "No Data Found"}
-        return Response(data, status=status.HTTP_400_BAD_REQUEST)
+        error_msg = {"Message": "No Data Found"}
+        return JsonResponse(error_msg, status=status.HTTP_400_BAD_REQUEST)
         # return Response(status=status.HTTP_400_BAD_REQUEST)
+
 
 @api_view(['POST'])
 def flat_create(request):
-    new_flat = FlatSerializer(data=request.data)
+    flat = FlatSerializer(data=request.data)
 
-    if new_flat.is_valid():
-        new_flat.save()
-        return Response(new_flat.data, status=status.HTTP_201_CREATED)
-    return Response(new_flat.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
+    if flat.is_valid():
+        flat.save()
+        return Response(flat.data, status=status.HTTP_201_CREATED)
+    return Response(flat.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
+@api_view(['PUT', 'PATCH'])
+def flat_update(request, pk):
+    try:
+        flat = Flat.objects.get(pk=pk)
+    except:
+        message = {
+            'message': 'Data does not exist'
+        }
+        return JsonResponse(message, status=status.HTTP_404_NOT_FOUND)
 
+    serializer = FlatSerializer(instance=flat, data=request.data, partial=True)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['DELETE'])
+def flat_delete(request, pk):
+    try:
+        flat = Flat.objects.get(pk=pk)
+        flat.delete()
+        message = {
+            'message': 'Item deleted Successfully!',
+        }
+        return JsonResponse(message, status=status.HTTP_200_OK)
+    except:
+        message = {
+            'message': 'Data does not exist'
+        }
+        return JsonResponse(message, status=status.HTTP_404_NOT_FOUND)
--- rms/settings.py
+++ rms/settings.py
@@ -19,7 +19,6 @@ environ.Env.read_env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -27,10 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -73,8 +71,8 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'rms.wsgi.application'
 
+WSGI_APPLICATION = 'rms.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
@@ -85,7 +83,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -105,7 +102,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
@@ -119,7 +115,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
@@ -129,3 +124,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
