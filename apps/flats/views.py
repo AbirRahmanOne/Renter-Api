1e6c8febafe9fbe05ca33f41ff1dd25490fceb20
AbirRahmanOne <abir.talks@gmail.com>
2021-09-15 15:34:09 +0600

    added feature permission

--- apps/flats/views.py
+++ apps/flats/views.py
@@ -5,27 +5,31 @@ from rest_framework.decorators import api_view
 from rest_framework.generics import *
 from rest_framework.permissions import IsAuthenticated
 from .serializers import *
+from apps.users.permissions import IsAdmin, IsModerator
 from .models import *
 
 
 # Create your views here.
 
 class FlatListAPIView(ListAPIView):
- #   permission_classes = [IsAuthenticated]
     queryset = Flat.objects.all()
     serializer_class = FlatSerializer
+    permission_classes = [IsAuthenticated | IsAdmin | IsModerator]
 
-#
+
 class FlatCreateAPIView(CreateAPIView):
+    permission_classes = [IsAuthenticated & IsAdmin]
     queryset = Flat.objects.all()
     serializer_class = FlatSerializer
 
 
 class FlatRetrieveUpdateAPIView(RetrieveUpdateAPIView):
+    permission_classes = [IsAuthenticated & IsAdmin]
     queryset = Flat.objects.all()
     serializer_class = FlatSerializer
 
 
 class FlatDeleteAPIView(DestroyAPIView):
+    permission_classes = [IsAuthenticated & IsAdmin]
     queryset = Flat.objects.all()
     serializer_class = FlatSerializer
